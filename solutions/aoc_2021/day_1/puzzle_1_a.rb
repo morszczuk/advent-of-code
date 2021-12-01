cae21530de7a408864348fe6611b916df11bc0c0
Miron Marczuk <miron@secret-source.eu>
2021-12-01 12:05:24 +0000

    2021 Day 1 - Improved

--- solutions/aoc_2021/day_1/puzzle_1_a.rb
+++ solutions/aoc_2021/day_1/puzzle_1_a.rb
@@ -2,11 +2,7 @@ module Aoc2021
   module Day1
     class Puzzle1A < Puzzle1
       def solve
-        increases = []
-        @measurements.each_cons(2) do |a, b|
-          increases << b if b > a
-        end
-        increases.count
+        @measurements.each_cons(2).count { |a, b| a < b }
       end
     end
   end
--- solutions/aoc_2021/day_1/puzzle_1_b.rb
+++ solutions/aoc_2021/day_1/puzzle_1_b.rb
@@ -2,15 +2,7 @@ module Aoc2021
   module Day1
     class Puzzle1B < Puzzle1
       def solve
-        group_sums = []
-        @measurements.each_cons(3) do |a, b, c|
-          group_sums << a + b + c
-        end
-        increases = 0
-        group_sums.each_cons(2) do |a, b|
-          increases += 1 if b > a
-        end
-        increases
+        @measurements.each_cons(3).map(&:sum).each_cons(2).count { |a, b| a < b }
       end
     end
   end
